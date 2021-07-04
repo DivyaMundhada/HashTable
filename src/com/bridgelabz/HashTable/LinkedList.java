4af47e1918fc4bd21bdfe713d28f61eac4e3742c
DivyaMundhada <divyamundada9@gmail.com>
2021-07-04 20:32:26 +0530

    Added Feature To Remove Avoidable Word From The Phrase.

--- src/com/bridgelabz/HashTable/LinkedList.java
+++ src/com/bridgelabz/HashTable/LinkedList.java
@@ -69,12 +69,26 @@ public class LinkedList<K, V> {
 		return index;
 	}
 
-	@Override
-	public String toString() {
-		return "MyLinkedListNodes{ " + head + " }";
+	public void remove(K word) {
+		MyMapNode currentNode = head;
+		MyMapNode previousNode = null;
+		while (currentNode != null && currentNode.getKey().equals(word)) {
+			head = currentNode.getNext();
+			return;
+		}
+		while (currentNode != null && !(currentNode.getKey().equals(word))) {
+			previousNode = currentNode;
+			currentNode = currentNode.getNext();
+		}
+		if (currentNode != null) {
+			previousNode.next = currentNode.next;
+		}
+		if (currentNode == null)
+			System.out.println("Word not found!");
 	}
 
-	public void printNodes() {
-		System.out.println("My nodes: " + head);
+	@Override
+	public String toString() {
+		return "MyLinkedListNodes{" + head + "}";
 	}
 }
